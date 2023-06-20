d5f44f4a168d00521a2f4c566fe8fc44a21bb0d0
nicerosebenny <117380574+nicerosebenny@users.noreply.github.com>
2023-06-20 20:39:41 +0530

    Add files via upload

---
+++ ScrapManagementBackend-main/Models/itemModel.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var itemSchema = mongoose.Schema({
+    uid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"userModel"
+    },
+    tid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"tokenModel"
+    },
+    category:{
+        type:String
+    },
+    
+   
+    quantity:{
+        type:Number
+    },
+    description:{
+        type:String
+    }
+    
+}, { 
+    //timestamps: true
+    timestamps: { createdAt: 'create_date', updatedAt: 'update_date' }
+});
+
+module.exports = mongoose.model('itemModel', itemSchemaSchema);
